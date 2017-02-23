13737f22cd4fe2fa1c6eeaa4e4421056c263daf9
neighbor983 <kenneth.ray.baker@gmail.com>
2017-02-23 06:39:07 +0000

    moved items into feature folders

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -11,10 +11,10 @@ import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroesComponent }      from './heroes.component';
-import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroService }          from './hero.service';
-import { HeroSearchComponent }  from './hero-search.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroService }          from './hero/hero.service';
+import { HeroSearchComponent }  from './hero-search/hero-search.component';
 
 @NgModule({
   imports: [
